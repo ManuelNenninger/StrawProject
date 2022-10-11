d59c1eb3d5c8af313a70f1d410ee1e79cc5cfb78
Manuel <manuel.nenninger@web.de>
2022-10-11 11:52:26 +0200

    seo Head und Datenschutz

--- pages/datenschutz.js
+++ pages/datenschutz.js
@@ -1,19 +1,9 @@
 import Datenschutz from "../src/components/templates/rechtliches/datenschutzerklaerung";
-import Box from "@mui/material/Box";
 
 export default function DatenschutzSite(){
   return(
     <>
-    <Box
-      sx={{
-        maxWidth: "1680px",
-        minHeight: "100vh",
-        p: 6,
-      }}
-    >
-      <p>Datenschutz</p>
-      </Box>
-      {/*<Datenschutz/>*/}
+      <Datenschutz/>
     </>
   )
 }
